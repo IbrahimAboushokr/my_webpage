fcb3dca03c39521970bfb941d047aa00d87363fb
Ibrahim Aboushokr GitHub <hshokr25@gmail.com>
2025-10-18 22:29:46 +0100

    Update main.js animation timing and rename CV file

--- js/main.js
+++ js/main.js
@@ -50,38 +50,24 @@ $(" .myP").show(500);
 $(document).ready(function () {
   let aboutSectionShown = false;
 
-  // مراقب السيكشن الأول
   const aboutObserver = new IntersectionObserver((entries, obs) => {
     entries.forEach(entry => {
       if (entry.isIntersecting && !aboutSectionShown) {
         aboutSectionShown = true;
         const aboutSection = $('#about');
-        aboutSection.removeClass('d-none').hide().fadeIn(500, () => {
+        aboutSection.removeClass('d-none').hide().fadeIn(1, () => {
 
-          // العنوان
           const h2 = $("#about h2");
-          h2.removeClass("d-none").addClass("from-left");
-          setTimeout(() => h2.addClass("in-view"), 250);
+          h2.removeClass("d-none").addClass("from-left in-view");
 
-          // الصورة
-          setTimeout(() => {
-            const img = $("#about img");
-            img.removeClass("d-none").addClass("from-left");
-            setTimeout(() => img.addClass("in-view"), 250);
+          const img = $("#about img");
+          img.removeClass("d-none").addClass("from-left in-view");
 
-            // البراجراف
-            setTimeout(() => {
-              const p = $("#about p");
-              const text = p.text();
-              p.text(text).removeClass("d-none").addClass("from-left");
-              setTimeout(() => p.addClass("in-view"), 250);
+          const p = $("#about p");
+          const text = p.text();
+          p.text(text).removeClass("d-none").addClass("from-left in-view");
 
-              // بعد الأول نشغل الثاني
-              setTimeout(() => showContactsSection(), 1000);
-            }, 1500);
-
-          }, 1500);
-
+          showContactsSection();
         });
         obs.unobserve(entry.target);
       }
@@ -90,94 +76,51 @@ $(document).ready(function () {
 
   aboutObserver.observe(document.querySelector('#about'));
 
-  // دالة لإظهار السيكشن الثاني
   function showContactsSection() {
     const contactsSection = $('#contacts');
-    contactsSection.removeClass('d-none').hide().fadeIn(750, () => {
+    contactsSection.removeClass('d-none').hide().fadeIn(1, () => {
 
       const h2 = contactsSection.find('h2');
-      h2.removeClass('d-none').addClass('from-left');
-      setTimeout(() => h2.addClass('in-view'), 300);
+      h2.removeClass('d-none').addClass('from-left in-view');
 
       const mainP = contactsSection.find('p').first();
       const mainText = mainP.text();
-      mainP.text(mainText).removeClass('d-none').addClass("from-left");
-      setTimeout(() => mainP.addClass('in-view'), 300);
+      mainP.text(mainText).removeClass('d-none').addClass("from-left in-view");
 
-      const card = contactsSection.find('.col-lg-6 .card').first();
-      const cardImg = card.find('img');
-      const cardBody = card.find('.card-body');
+      const cards = contactsSection.find('.col-lg-6 .card');
+      cards.removeClass('d-none').hide().fadeIn(1, () => {
+        cards.find('img, .card-body').removeClass('d-none').addClass('from-left in-view');
 
-      setTimeout(() => {
-        card.removeClass('d-none').hide().fadeIn(500, () => {
-          cardImg.removeClass('d-none').addClass('from-left');
-          setTimeout(() => cardImg.addClass('in-view'), 250);
-
-          cardBody.removeClass('d-none').addClass('from-left');
-          setTimeout(() => cardBody.addClass('in-view'), 400);
-
+        // اللينكات الخمسة (GitHub, CV, Facebook, WhatsApp, Instagram)
+        const links = cards.find('.list-group a');
+        links.each(function (index) {
+          const link = $(this);
           setTimeout(() => {
-            const card2 = contactsSection.find('.col-lg-6 .card').last();
-            const card2Body = card2.find('.card-body');
-            const links = card2.find('.list-group a');
-
-            card2.removeClass('d-none').hide().fadeIn(500, () => {
-              card2Body.removeClass('d-none').addClass('from-left in-view');
-
-              links.each(function(index) {
-                const link = $(this);
-                link.removeClass('d-none').addClass('icon-from-left');
-                setTimeout(() => link.addClass('in-view'), 300 + index * 300);
-              });
-
-              setTimeout(() => showCommunicationSection(), 1500);
-            });
-
-          }, 1000);
-
+            link.removeClass('d-none').addClass('icon-from-left in-view');
+          }, 150 * index); // كل لينك بعد 150ms من اللي قبله
         });
-      }, 1000);
 
+        showCommunicationSection();
+      });
     });
   }
 
-  // دالة لإظهار السيكشن الثالث
   function showCommunicationSection() {
     const commSection = $('#communication');
-    commSection.removeClass('d-none').hide().fadeIn(1000, () => {
-
-      const h2 = commSection.find('h2');
-      h2.removeClass('d-none').addClass('from-left');
-      setTimeout(() => h2.addClass('in-view'), 400);
-
-      const mainP = commSection.find('p').first();
-      const mainText = mainP.text();
-      mainP.text(mainText).removeClass('d-none').addClass('from-left');
-      setTimeout(() => mainP.addClass('in-view'), 400);
-
-      setTimeout(() => {
-        const infoDiv = commSection.find('.col-12.col-md-6').first().find('div');
-        infoDiv.removeClass('d-none').addClass('from-left');
-        setTimeout(() => infoDiv.addClass('in-view'), 500);
-      }, 1000);
-
-      setTimeout(() => {
-        const mapDiv = commSection.find('.col-12.col-md-6').last().find('div');
-        mapDiv.removeClass('d-none').addClass('from-left');
-        setTimeout(() => mapDiv.addClass('in-view'), 750);
-      }, 1500);
-
-      setTimeout(() => showSliderSection(), 2500);
+    commSection.removeClass('d-none').hide().fadeIn(1, () => {
+      commSection.find('h2, p, .col-12.col-md-6 div')
+        .removeClass('d-none')
+        .addClass('from-left in-view');
+      showSliderSection();
     });
   }
 
-  // دالة لإظهار السيكشن الرابع
   function showSliderSection() {
     const sliderSection = $('section.m-2');
-    sliderSection.removeClass('d-none').hide().fadeIn(1000, () => {
-      const carousel = sliderSection.find('#carouselExampleIndicators');
-      carousel.removeClass('d-none').addClass('from-left');
-      setTimeout(() => carousel.addClass('in-view'), 500);
+    sliderSection.removeClass('d-none').hide().fadeIn(1, () => {
+      sliderSection.find('#carouselExampleIndicators')
+        .removeClass('d-none')
+        .addClass('from-left in-view');
     });
   }
 
